709cfa6cafcdee12a9064b0ad53f85c03b73ee25
yongcrane96 <dydgkr3363@naver.com>
2025-03-25 09:29:28 +0900

    fix -  uuid -> snowflakeId 사용

--- src/main/java/SoloTech/StockFlow/order/service/OrderService.java
+++ src/main/java/SoloTech/StockFlow/order/service/OrderService.java
@@ -3,10 +3,11 @@ package SoloTech.StockFlow.order.service;
 import SoloTech.StockFlow.order.dto.OrderDto;
 import SoloTech.StockFlow.order.entity.Order;
 import SoloTech.StockFlow.order.repository.OrderRepository;
+import cn.hutool.core.lang.Snowflake;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
-import org.aspectj.weaver.ast.Or;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
@@ -25,10 +26,17 @@ public class OrderService {
     private final OrderRepository orderRepository;
     private final ObjectMapper mapper;
 
+    @Transactional
     public Order createOrder(OrderDto dto) {
         Order order = mapper.convertValue(dto, Order.class);
-        String uuid = UUID.randomUUID().toString();
-        order.setOrderId(uuid);
+
+        // Snowflake ID 생성
+        Snowflake snowflake = new Snowflake(1,1);
+        long snowflakeId = snowflake.nextId();
+
+//       String uuid = UUID.randomUUID().toString();
+//       order.setOrderId(uuid);
+        order.setOrderId(String.valueOf(snowflakeId));
         return orderRepository.saveAndFlush(order);
     }
 
@@ -37,6 +45,7 @@ public class OrderService {
                 .orElseThrow(()->new RuntimeException("Order not found: " + orderId));
     }
 
+    @Transactional
     public Order updateOrder(String orderId, OrderDto dto) throws JsonMappingException {
         Order order = this.readOrder(orderId);
         mapper.updateValue(order, dto);
