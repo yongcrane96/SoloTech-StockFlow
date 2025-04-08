71fc40cda892e54ae53919894b07c3c8e6a0949a
yongcrane96 <dydgkr3363@naver.com>
2025-04-08 19:29:01 +0900

    cached 테스트 코드 수정

--- src/main/java/SoloTech/StockFlow/order/dto/OrderDto.java
+++ src/main/java/SoloTech/StockFlow/order/dto/OrderDto.java
@@ -2,6 +2,7 @@ package SoloTech.StockFlow.order.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 
 /**
@@ -10,6 +11,7 @@ import lombok.Data;
  * @since   2025-03-18
  * @author  yhkim
  */
+@Builder
 @Data
 @AllArgsConstructor
 public class OrderDto {
