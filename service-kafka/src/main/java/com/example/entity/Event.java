2daf7449b22202360a5a0f681542ebd14f9e3ce8
yongcrane96 <dydgkr3363@naver.com>
2025-04-23 17:23:14 +0900

    feature-Kafka 기능 추가

---
+++ service-kafka/src/main/java/com/example/entity/Event.java
@@ -0,0 +1,36 @@
+package com.example.entity;
+
+import lombok.*;
+
+import java.io.Serializable;
+
+/**
+ * Event 객체
+ *
+ * Kafka 메시지로 보내는 데이터 구조를 정의한 클래스입니다.
+ * - type: 메시지의 유형을 나타내는 문자열
+ * - payload: 메시지의 실제 내용을 담고 있는 문자열
+ * @since   2025-04-23
+ * @author  yhkim
+ */
+@Setter
+@Data
+public class Event implements Serializable { // Serializable을 구현함으로써 바이트 스트림으로 변환 가능
+    private int id;
+    private String type;
+    private String payload;
+
+    public Event() {}
+
+    /**
+     * 생성자: type과 payload를 초기화하는 생성자
+     *
+     * @param type     메시지 유형
+     * @param payload  메시지 내용
+     */
+    public Event(int id, String type, String payload){
+        this.id = id;
+        this.type = type;
+        this.payload = payload;
+    }
+}
