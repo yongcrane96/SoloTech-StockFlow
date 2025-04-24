fa910d46b5ea5c9f5853e91ed97e9db529c9a3cd
yongcrane96 <dydgkr3363@naver.com>
2025-04-24 21:53:09 +0900

    멀티 모듈에 기존 scr 제거

--- src/main/java/SoloTech/StockFlow/payment/entity/PaymentStatus.java
+++
@@ -1,8 +0,0 @@
-package SoloTech.StockFlow.payment.entity;
-
-public enum PaymentStatus {
-    PENDING,   // 결제 대기
-    PAID,      // 결제 완료
-    CANCELED,   // 결제 취소
-    Success
-}
--- src/main/java/SoloTech/StockFlow/product/exception/ProductNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package SoloTech.StockFlow.product.exception;
-
-import SoloTech.StockFlow.common.util.BusinessException;
-
-public class ProductNotFoundException extends BusinessException {
-    public ProductNotFoundException(String productId) {
-        super("Product not found: " + productId);
-    }
-}
