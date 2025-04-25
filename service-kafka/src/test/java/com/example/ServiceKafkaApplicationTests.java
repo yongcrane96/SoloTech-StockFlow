0f129eb9b18d2ff2d9fab95b92bf4ce30a6171d3
yongcrane96 <dydgkr3363@naver.com>
2025-04-25 11:38:58 +0900

    kafkaApplicationTest 수정

--- service-kafka/src/test/java/com/example/ServiceKafkaApplicationTests.java
+++ service-kafka/src/test/java/com/example/ServiceKafkaApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class ServiceKafkaApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package com.example;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class ServiceKafkaApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
