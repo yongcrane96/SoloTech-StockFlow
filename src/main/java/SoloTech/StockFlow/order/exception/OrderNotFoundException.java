59b31f94c8eade8b4ed35d36a5ac4bd543707edb
yongcrane96 <dydgkr3363@naver.com>
2025-05-02 12:42:35 +0900

    fix - src-소스-제거
    fix - src-소스-제거

--- src/main/java/SoloTech/StockFlow/order/exception/OrderNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package SoloTech.StockFlow.order.exception;
-
-import SoloTech.StockFlow.common.util.BusinessException;
-
-public class OrderNotFoundException extends BusinessException {
-    public OrderNotFoundException(String orderId){
-        super("Order not found: " + orderId);
-    }
-}
